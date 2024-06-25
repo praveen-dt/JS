03ab133d7a5349c33e61ac8780d990a4065c4c50
Praveen-dt <dangoria.praveen@gmail.com>
2024-06-25 05:43:37 -0500

    stack and heap

--- 02-datatypes.js
+++ 02-datatypes.js
@@ -5,5 +5,50 @@ let isLoggedin = false //bollean
 let state = null  //stand alone
 let states;  //undefined
 
-console.log(typeof(age))
+//console.log(typeof(age))
 
+
+
+const array = ['abc','bcd']
+let obj = {
+    name : "don",
+    age : 25
+}
+
+const ggwala = function(){
+    console.log("hero")
+}
+
+
+
+//###################
+//Stack and Heap
+
+//stack (promitive), heap (non primitive)
+
+let firstname = "hero"
+let name1 = firstname
+
+console.log(firstname)
+console.log(name1)
+
+name1 = "done"
+console.log(firstname)
+console.log(name1)
+
+
+//++++++++++++++++++++
+
+let user1 = {
+    email : "hero@hotmail.com",
+    age : 35
+}
+
+let user2 =  user1
+
+console.log(user1)
+console.log(user2)
+
+user2.email = "mango@kyu.io"
+console.log(user1)
+console.log(user2)
