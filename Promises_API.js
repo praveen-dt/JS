03b33a5deff8c88a2f91186ce447546c8dd8db12
Praveen-dt <dangoria.praveen@gmail.com>
2024-06-27 12:14:37 -0500

    more on promise asic and await

--- Promises_API.js
+++ Promises_API.js
@@ -23,3 +23,23 @@ new Promise(function(resolve, reject){
     console.log("Task 2 resolved")
 })
 
+//Anoterh way with some error 
+const promis4 = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let err = true
+        if(!err){
+            resolve({username: "namo", password: "1234"})
+        }else{
+            reject("Error: Something went wrong")
+        }
+    }, 1000)
+})
+
+promis4.then((user) => {
+    console.log(user)
+    return user.username
+}).then((username) => {
+    console.log(username)
+}).catch(function(err){
+    console.log(err)
+}).finally(() => console.log("Promsie is either resolved or rejected"))
